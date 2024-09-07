03c4a75ef5027f6ffa09c77f8b4b8b65866899bf
Xiaotian Li <xiaotian.li@lapisit.com.au>
2024-09-07 17:17:03 +1000

    Add basic key value store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,14 @@ import { routes } from './app.routes';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 import {provideStore} from '@ngrx/store';
 import {keyValueReducer} from "./data-access/store/key-value/key-value.reducer";
+import {provideStoreDevtools} from "@ngrx/store-devtools";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideIonicAngular({}),
+    provideStoreDevtools({ maxAge: 25 }),
     provideStore({
       keyValue: keyValueReducer
     }),
--- src/app/page/category-filter/category-filter.component.ts
+++ src/app/page/category-filter/category-filter.component.ts
@@ -1,7 +1,12 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {IonicModule} from "@ionic/angular";
 import {CommonModule} from "@angular/common";
 import {AppRangeExample} from "../../component/slides-demo/app-range-example";
+import {Router} from "@angular/router";
+import {Store} from "@ngrx/store";
+import {setKeyValue} from "../../data-access/store/key-value/key-value.actions";
+import {DYNAMIC_FILTER_RESULT_KEY} from "../../data-access/constant/key-value-storeage.constants";
+import {selectValueByKey} from "../../data-access/store/key-value/key-value.selectors";
 
 @Component({
   selector: 'app-category-filter',
@@ -14,9 +19,16 @@ export class CategoryFilterComponent  implements OnInit {
 
   @ViewChild(AppRangeExample) rangeComponent!: AppRangeExample;
 
+  private router = inject(Router);
+  private store = inject(Store);
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.store.select(selectValueByKey(DYNAMIC_FILTER_RESULT_KEY)).subscribe((value) => {
+      console.log(value)
+    });
+  }
 
   ranges = [
     { label: 'Range 1', value: 25, locked: false, ignore: false },
@@ -34,6 +46,9 @@ export class CategoryFilterComponent  implements OnInit {
   }
 
   onApplyButtonClicked() {
-    console.log('Ranges:', this.ranges);
+    this.store.dispatch(
+      setKeyValue({ key:DYNAMIC_FILTER_RESULT_KEY, value: this.ranges })
+    )
+    this.router.navigate(['/home']);
   }
 }
