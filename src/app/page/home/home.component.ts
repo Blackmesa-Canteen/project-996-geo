53ab85d1542c38d24d9511a0532a0fe893f710db
Krishna Dere <krishna.dere@spatialvision.com.au>
2024-09-07 17:08:31 +1000

    add: leaflet map

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import {IonicModule} from "@ionic/angular";
-import {CommonModule} from "@angular/common";
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule]
+  imports: [CommonModule, IonicModule],
 })
-export class HomeComponent  implements OnInit {
+export class HomeComponent implements OnInit {
+  map!: L.Map;
 
-  constructor() { }
+  ngOnInit() {
+    this.initializeMap();
+  }
 
-  ngOnInit() {}
+  initializeMap() {
+    this.map = L.map('map', {
+      center: [-37, 145], // Set the map center coordinates
+      zoom: 13, // Set the initial zoom level
+    });
 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(this.map);
+  }
 }
