4b85d284c6d0e4a90154f21df32b9be634a7a655
Xiaotian Li <xiaotian.li@lapisit.com.au>
2024-09-07 18:01:29 +1000

    add feature details page

--- src/app/component/feature-detail/feature-detail.component.ts
+++ src/app/component/feature-detail/feature-detail.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
-import {IonicModule} from "@ionic/angular";
+import {IonicModule, ModalController, NavController} from "@ionic/angular";
 import {Store} from "@ngrx/store";
 import {selectValueByKey} from "../../data-access/store/key-value/key-value.selectors";
 import {FEATURE_DETAILS_KEY} from "../../data-access/constant/key-value-storeage.constants";
@@ -19,6 +19,8 @@ import {setKeyValue} from "../../data-access/store/key-value/key-value.actions";
 export class FeatureDetailComponent implements OnInit {
 
   private store = inject(Store);
+  private readonly navController = inject(NavController);
+  private readonly modalController = inject(ModalController);
 
   protected featureDetail$: Observable<IFeatureDetailsData> = this.store.select(selectValueByKey(FEATURE_DETAILS_KEY)).pipe(
     filter(isNotNil)
@@ -30,5 +32,9 @@ export class FeatureDetailComponent implements OnInit {
         title: 'test',
         description: 'test des',
     }}));
+  }
+
+  handleExit = async () => {
+    await this.modalController.dismiss();
   }
 }
