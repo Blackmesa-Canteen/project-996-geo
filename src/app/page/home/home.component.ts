5f7aa2e9e7a9e074183958ccd544e2d552a379e7
Xiaotian Li <xiaotian.li@lapisit.com.au>
2024-09-08 12:19:31 +1000

    Add heatmap demo

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, OnInit} from '@angular/core';
 import {IonicModule, ModalController} from '@ionic/angular';
 import {CommonModule} from '@angular/common';
 import * as L from 'leaflet';
+import 'leaflet.heat';
 import {HttpClient} from '@angular/common/http';
 import {FeatureDetailComponent} from "../../component/feature-detail/feature-detail.component";
 import {LeafletModule} from "@asymmetrik/ngx-leaflet";
@@ -12,6 +13,7 @@ import {DYNAMIC_FILTER_RESULT_KEY} from "../../data-access/constant/key-value-st
 import {selectValueByKey} from "../../data-access/store/key-value/key-value.selectors";
 import {isNotNil} from "rambda";
 import {FormsModule} from "@angular/forms";
+import {HeatMapOptions} from "leaflet";
 
 @Component({
   selector: 'app-home',
@@ -23,6 +25,7 @@ import {FormsModule} from "@angular/forms";
 
 export class HomeComponent implements OnInit {
   protected map!: L.Map;
+  private heatLayer?: L.Layer;
 
   private store = inject(Store);
   private http = inject(HttpClient);
@@ -70,19 +73,70 @@ export class HomeComponent implements OnInit {
    * @param event
    * @param range
    */
-  onDynamicFilterSideMenuCheckboxChange(event: any, range: any) {
+  protected onDynamicRangesMenuCheckboxChange(event: any, range: any) {
     console.log('Checkbox checked:', event.detail.checked);
     console.log('Label:', range.label);
   }
 
+  /**
+   * Will listen to the dynamic filter data changes
+   * @param value
+   * @constructor
+   * @private
+   */
+  private OnDynamicRangesChanged(value: any) {
+    if (isNotNil(value)) {
+      this.ranges = value
+        .filter((range: any) => !range.ignore)
+        .sort((a: any, b: any) => b.value - a.value)
+        .map((range: any) => ({...range}));
+
+      // TODO: render the heatmap data accordingly
+      this.renderHeatmapData();
+    }
+  }
+
+  /**
+   * Use this to render the heatmap data on the map
+   * Currently, it's just random data for now
+   * @private
+   */
+  private renderHeatmapData() {
+    const center = L.latLng({ lat: -37, lng: 145 });
+    const points: L.HeatLatLngTuple[] = [];
+    const numPoints = 1000; // Number of random points
+
+    for (let i = 0; i < numPoints; i++) {
+      const lat = center.lat + (Math.random() - 0.5) * 2; // Larger random latitude range
+      const lng = center.lng + (Math.random() - 0.5) * 2; // Larger random longitude range
+      const intensity = Math.random(); // Random intensity value
+      points.push([lat, lng, intensity]);
+    }
+
+    const heatMapOptions: L.HeatMapOptions = {
+      radius: 60, // Increased radius
+      blur: 40,   // Increased blur
+      maxZoom: 17,
+      max: 1.0,   // Maximum intensity value
+      gradient: { 0.4: 'blue', 0.65: 'lime', 1: 'red' } // Custom gradient
+    };
+
+    if (this.map) {
+      // Remove existing heatmap layer if it exists
+      if (this.heatLayer) {
+        this.map.removeLayer(this.heatLayer);
+      }
+
+      // Add the new heatmap layer to the map
+      this.heatLayer = L.heatLayer(points, heatMapOptions).addTo(this.map);
+    }
+  }
+
+
+
   private initSideMenuLayerLabels() {
     this.store.select(selectValueByKey(DYNAMIC_FILTER_RESULT_KEY)).subscribe((value) => {
-      if (isNotNil(value)) {
-        this.ranges = value
-          .filter((range: any) => !range.ignore)
-          .sort((a: any, b: any) => b.value - a.value)
-          .map((range: any) => ({...range}));
-      }
+      this.OnDynamicRangesChanged(value);
     });
   }
 
